4d5de98264d485bb370e7963113993df1f12c149
nitish0104 <nitishdalvi1@gmail.com>
2023-03-07 20:59:52 +0530

    qrcode data

--- src/context/Auth_context.js
+++ src/context/Auth_context.js
@@ -1,28 +1,45 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../Firebase_config';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { auth, db } from '../Firebase_config';
 
 
 
 const AuthContext = createContext()
 const AuthContextProvider = ({ children }) => {
-	// const [user, setuser] = useState()
+	const [user, setuser] = useState()
 	const [QRstate, setQRstate] = useState("");
+
 	const navigate = useNavigate()
+
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				const uid = user.uid
+				console.log(uid)
+				setuser(user)
+			} else {
+			}
+		});
+	}, [])
 
 	const handleSignup = async (fromstate) => {
 		await createUserWithEmailAndPassword(auth, fromstate.email, fromstate.password)
 			.then((userCredential) => {
 				// Signed in 
 				const user = userCredential.user;
-				// setuser(user)
+				setDoc(doc(db, "Teachers", user.uid), {
+					email: user.email,
+					uid: user.uid
+				})
+				setuser(user)
 				console.log(user)
 				console.log(user.uid)
 				// ...
 			}).then(() => {
 				alert("account created sucessfully")
-				navigate("/")
+				navigate("/main")
 			}).catch((err) => {
 				console.error(err);
 				alert(err)
@@ -33,9 +50,7 @@ const AuthContextProvider = ({ children }) => {
 			.then((userCredential) => {
 				// Signed in
 				const user = userCredential.user;
-				console.log(user);
-
-				// ...
+				console.log(user)
 			})
 			.then(() => {
 				alert("login sucessfully")
@@ -49,13 +64,14 @@ const AuthContextProvider = ({ children }) => {
 			});
 	};
 	const generateqrcode = (fromstate) => {
+
 		let currentDate = new Date();
 		let currentDay = currentDate.getDate()
 		let month = currentDate.getMonth() + 1
 		let year = currentDate.getFullYear()
 		let finalDate = `${currentDay}-${month}-${year}`
 		let obj = {
-
+			uid: user.uid,
 			...fromstate,
 			date: finalDate
 		}
