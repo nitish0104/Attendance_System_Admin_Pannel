88e8d4e4ff96c5d9eb643299d5a8496402576c25
Prakash8999 <reshavejha@gmail.com>
2023-04-06 23:06:37 +0530

    Date and Time Added, No changes with fb

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const input = ({ label, type, placeholder, value, onChange, id }) => {
+const input = ({ label, type, placeholder, value, onChange, id, className }) => {
 	return (
 		<div className='flex flex-col '>
 
 			<label className=' font-bold'>{label}
 			</label>
-			<input type={type} placeholder={placeholder} id={id} onChange={onChange} value={value} className="rounded-lg border-2 border-gray-900 p-1   focus:outline-blue-800 focus:text-black " autoComplete="on" />
+			<input type={type} placeholder={placeholder} id={id} onChange={onChange} value={value} className={`rounded-lg border-2 border-gray-900 p-1   focus:outline-blue-800 focus:text-black  autoComplete="on" ${className}`} />
 		</div>
 	)
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,103 @@
-import Input from "../components/Input";
+// import Input from "../components/Input";
+// import React, { useState } from "react";
+// import Layout from "../components/Layout";
+// import Select from "../components/Select";
+// import { option_department, option_year, subjects } from "../components/Data";
+// import { UserAuth } from "../context/Auth_context";
+// import QRCode from "react-qr-code";
+
+// const Main = () => {
+//   const { generateqrcode, QRstate, user } = UserAuth();
+//   const initialstate = {
+//     tname: "",
+//     subname: "",
+//     department: "",
+//     year: "",
+//   };
+//   const [formstate, setformstate] = useState(initialstate);
+
+//   const handlechange = (e) => {
+//     setformstate({
+//       ...formstate,
+//       [e.target.id]: e.target.value,
+//     });
+//   };
+
+//   return (
+//     <Layout>
+//       <div className="absolute w-[70vw] h-[90vh] bg-white flex flex-col items-center justify-center rounded-lg gap-y-5">
+//         <form action="" className="contents w-[100vw]">
+//           <Input
+//             label="Teacher's Name:"
+//             value={formstate.tname}
+//             id="tname"
+//             placeholder="Enter Your Name"
+//             type="text"
+//             onChange={handlechange}
+//           ></Input>
+//           <Select
+//             id="department"
+//             label="Department"
+//             value={formstate.department}
+//             options={option_department}
+//             onChange={handlechange}
+//           ></Select>
+//           <Select
+//             id="year"
+//             label="Year"
+//             value={formstate.year}
+//             options={option_year}
+//             onChange={handlechange}
+//           ></Select>
+//           <Select
+//             id="subname"
+//             label="Subject Name:"
+//             value={formstate.subname}
+//             options={subjects}
+//             onChange={handlechange}
+//           ></Select>
+          
+//           <button
+//             onClick={() => generateqrcode(formstate, user)}
+//             type="button"
+//             className="bg-purple-500 px-4 py-2 rounded-lg hover:bg-purple-600"
+//           >
+//             Generate QR
+//           </button>
+//           <div className="flex justify-center items-center ">
+//             <QRCode
+//               size={256}
+//               className="vh-[30%] vw-[30%]"
+//               value={QRstate}
+//               viewBox={`0 0 256 256`}
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     </Layout>
+//   );
+// };
+
+// export default Main;
 import React, { useState } from "react";
 import Layout from "../components/Layout";
 import Select from "../components/Select";
 import { option_department, option_year, subjects } from "../components/Data";
 import { UserAuth } from "../context/Auth_context";
 import QRCode from "react-qr-code";
-
+import { useNavigate } from "react-router-dom";
+import Input from "../components/Input";
 const Main = () => {
   const { generateqrcode, QRstate, user } = UserAuth();
+  const navigate = useNavigate();
   const initialstate = {
     tname: "",
     subname: "",
     department: "",
     year: "",
+    date: "",
+    starttime:"",
+    endtime:"",
   };
   const [formstate, setformstate] = useState(initialstate);
 
@@ -23,18 +108,27 @@ const Main = () => {
     });
   };
 
+  const attendance = () => {
+    navigate("/attendance");
+  };
+
   return (
     <Layout>
-      <div className="absolute w-[70vw] h-[90vh] bg-white flex flex-col items-center justify-center rounded-lg gap-y-5">
+      <div className="flex ">
+
+      <div >
+        
+      <div className=" w-[60vw] h-[90vh]  flex flex-col items-center justify-center rounded-lg gap-y-5">
         <form action="" className="contents w-[100vw]">
-          <Input
+        <Input
             label="Teacher's Name:"
             value={formstate.tname}
             id="tname"
             placeholder="Enter Your Name"
             type="text"
             onChange={handlechange}
-          ></Input>
+            className={'w-[30vw]'}
+          ></Input> 
           <Select
             id="department"
             label="Department"
@@ -55,25 +149,63 @@ const Main = () => {
             value={formstate.subname}
             options={subjects}
             onChange={handlechange}
-          ></Select>
-          
-          <button
-            onClick={() => generateqrcode(formstate, user)}
-            type="button"
-            className="bg-purple-500 px-4 py-2 rounded-lg hover:bg-purple-600"
-          >
-            Generate QR
-          </button>
-          <div className="flex justify-center items-center ">
+            ></Select>
+          <Input
+          id=  "date"
+          label="Select date"
+          value={formstate.date}
+          onChange={handlechange}
+          type="date"
+          className={'w-[30vw]'}
+          />
+          <Input
+          id=  "starttime"
+          label="Select Start Time"
+          value={formstate.starttime}
+          onChange={handlechange}
+          type="time"
+className={'w-[30vw]'}
+          />
+          <Input
+          id=  "endtime"
+          label="Select End Time"
+          value={formstate.endtime}
+          onChange={handlechange}
+          type="time"
+          className={'w-[30vw]'}
+          />
+
+          <div className="flex items-center justify-between">
+            <button
+              onClick={() => generateqrcode(formstate, user)}
+              type="button"
+              className="bg-purple-500 px-4 py-2 rounded-lg hover:bg-purple-600 mr-20"
+              >
+              Generate QR
+            </button>
+            <button
+              onClick={attendance}
+              type="button"
+              className="bg-purple-500 px-4 py-2 rounded-lg hover:bg-purple-600 ml-20"
+              >
+              Attendance
+            </button>
+          </div>
+
+            </form>
+            </div>
+      </div>
+
+
+          <div className="flex justify-center items-center pr-12 pb-10">
             <QRCode
-              size={256}
-              className="vh-[30%] vw-[30%]"
+              size={350}
+              className="vh-[50%] vw-[50%]"
               value={QRstate}
               viewBox={`0 0 256 256`}
             />
           </div>
-        </form>
-      </div>
+              </div>
     </Layout>
   );
 };
